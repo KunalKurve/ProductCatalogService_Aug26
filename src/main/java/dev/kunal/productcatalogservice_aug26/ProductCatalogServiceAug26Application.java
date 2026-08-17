de36f27eb2e4a7053aa15ac51aca8c4860ab016d
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-08-17 21:12:39 +0530

    Created new Project

---
+++ src/main/java/dev/kunal/productcatalogservice_aug26/ProductCatalogServiceAug26Application.java
@@ -0,0 +1,13 @@
+package dev.kunal.productcatalogservice_aug26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogServiceAug26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogServiceAug26Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kunal/productcatalogservice_aug26/ProductCatalogServiceAug26ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kunal.productcatalogservice_aug26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceAug26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
