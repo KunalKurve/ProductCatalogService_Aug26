503c137c3e50a1376927d85167c91844d66a69d7
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-08-24 18:45:43 +0530

    Added changes for creating Entities out of Model classes

--- src/main/java/dev/kunal/productcatalogservice_aug26/models/BaseModel.java
+++ src/main/java/dev/kunal/productcatalogservice_aug26/models/BaseModel.java
@@ -1,6 +1,8 @@
 package dev.kunal.productcatalogservice_aug26.models;
 
 import dev.kunal.productcatalogservice_aug26.models.enums.State;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +10,10 @@ import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class BaseModel {
 
+    @Id
     private Long id;
 
     private Date createdAt;
--- src/main/java/dev/kunal/productcatalogservice_aug26/models/Product.java
+++ src/main/java/dev/kunal/productcatalogservice_aug26/models/Product.java
@@ -1,18 +1,29 @@
 package dev.kunal.productcatalogservice_aug26.models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
 
     private String name;
+
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
+
     private String description;
+
     private Double price;
+
     private Integer quantity;
+
     private String imageUrl;
-    private Boolean isSaleEligible;
+
+    private Boolean isSaleEligible; //This is why we require dto
 
 }
