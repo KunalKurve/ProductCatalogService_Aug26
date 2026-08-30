d1790dd403950c7c14eae6eecc85271ae9fb715a
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-08-30 21:25:14 +0530

    Added global exception handler for ProductAlreadyExistsException

--- src/main/java/dev/kunal/productcatalogservice_aug26/controllers/ControllerAdvisor.java
+++ src/main/java/dev/kunal/productcatalogservice_aug26/controllers/ControllerAdvisor.java
@@ -1,5 +1,6 @@
 package dev.kunal.productcatalogservice_aug26.controllers;
 
+import dev.kunal.productcatalogservice_aug26.exceptions.ProductAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice // This makes my exception handlers global
 public class ControllerAdvisor {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    private ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+    @ExceptionHandler({IllegalArgumentException.class})
+    private ResponseEntity<String> handleIllegalArgumentException(Exception e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RuntimeException.class)
     private ResponseEntity<String> handleRuntimeException(RuntimeException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    private ResponseEntity<String> handleProductAlreadyExistsException(ProductAlreadyExistsException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
     }
 }
 
